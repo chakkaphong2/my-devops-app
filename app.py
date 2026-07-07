797defc90adbce8a821b72a34cfd778914b4fdfd
DUP <chakkaphong23dungupa@gmail.com>
2026-07-08 03:02:14 +0700

    Update message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify(message="สวัสดี! นี่คือ API ตัวแรกของฉันที่รันบน Docker")
+    return jsonify(message="ฉันจะหนีทุกอย่างแหละจะเก่งขึ้น")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
